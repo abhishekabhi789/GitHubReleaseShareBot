116b745b22784a62f56f0c77194f3181df1d1a6e
Abhishek <abhishekabhi9240@gmail.com>
2024-03-23 12:04:36 +0530

    initial commit polling

---
+++ Bot.py
@@ -0,0 +1,88 @@
+import os
+import logging
+from telebot import TeleBot
+from telebot import telebot, types
+from Utils import log
+from GitHubSearch import get_inline_data, get_message_url_and_buttons_for
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = TeleBot(BOT_TOKEN,threaded= False)
+BOT_USERNAME = bot.get_me().username
+
+telebot.logger.setLevel(logging.INFO)
+
+
+# send welcome message
+def send_welcome(message):
+    buttons = types.InlineKeyboardMarkup()
+    btn_here = types.InlineKeyboardButton(
+        text="Go inline here", switch_inline_query_current_chat=""
+    )
+    btn_there = types.InlineKeyboardButton(
+        text="Go inline in a chat",
+        switch_inline_query_chosen_chat=types.SwitchInlineQueryChosenChat(
+            query="",
+            allow_bot_chats=True,
+            allow_channel_chats=True,
+            allow_group_chats=True,
+            allow_user_chats=True,
+        ),
+    )
+    buttons.row(btn_here, btn_there)
+    bot.reply_to(
+        message,
+        text=f"Hello {message.from_user.first_name},\nThis bot works only in inline mode.",
+        reply_markup=buttons,
+    )
+
+
+# handle commands
+@bot.message_handler(commands=["help", "start"])
+def handle_commands(message):
+    send_welcome(message)
+
+
+# All text message handled here
+@bot.message_handler(func=lambda message: True)
+def received_message(message):
+    # don't reply to own messages
+    if message.via_bot is not None and message.via_bot.username == BOT_USERNAME:
+        return
+    else:
+        send_welcome(message)
+
+
+# handle inline queries
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def query_text(inline_query):
+    id = str(inline_query.id)
+    query = inline_query.query
+    log(f"searching for {query}")
+    results = get_inline_data(query)
+    if results:
+        button = None
+    else:
+        button = types.InlineQueryResultsButton(
+            "No result found!", start_parameter="help"
+        )
+        log("No Result Found for " + query)
+    bot.answer_inline_query(id, results, button=button)
+
+
+@bot.chosen_inline_handler(func=lambda chosen_inline_result: True)
+def update_message_with_latest_release(chosen):
+    id = chosen.inline_message_id
+    message, buttons = get_message_url_and_buttons_for(chosen.result_id)
+    if message is not None and buttons is not None:
+        bot.edit_message_text(
+            text=message, inline_message_id=id, parse_mode="html", reply_markup=buttons
+        )
+    else:
+        log("failed to get chosen result details. Id: "+id)
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
---
+++ GitHubSearch.py
@@ -0,0 +1,131 @@
+import requests
+from Utils import log, parse_text_for_tg_markdown
+import urllib.parse
+from telebot import types
+from uuid import uuid4
+
+GITHUB_API = "https://api.github.com"
+REPO_SEARCH_ENDPOINT = GITHUB_API + "/search/repositories"
+REPO_RELEASE_ENDPOINT = GITHUB_API + "/repos"
+
+
+# these dictonaries are needed later to edit message
+message_dict = {}
+full_name_dict = {}
+buttons_dict = {}
+
+
+# get repos for the input
+def search_repos(query):
+    encoded_query = urllib.parse.quote(query)
+    url = f"{REPO_SEARCH_ENDPOINT}?q={encoded_query}"
+    try:
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            if data["total_count"] > 0:
+                log(f"Found {data['total_count']} repos for \"{query}\"")
+                return data["items"][:50]
+            else:
+                log(f"Not 200. {response.text()}")
+    except requests.RequestException as e:
+        log(f"Error fetching repos: {e}")
+    return None
+
+
+def get_release_details(repo):
+    url = f"{REPO_RELEASE_ENDPOINT}/{repo}/releases"
+    try:
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            return data
+        else:
+            log(f"Not 200. {response.text()}")
+    except requests.RequestException as e:
+            log(f"Error fetching releases: {e}")
+    return None
+
+
+def get_buttons(query, repo):
+    buttons = types.InlineKeyboardMarkup(row_width=3)
+    btn_retry = types.InlineKeyboardButton(
+        "Search again", switch_inline_query_current_chat=query
+    )
+    btn_repo = types.InlineKeyboardButton("View Repo", url=repo)
+    buttons.row(btn_retry, btn_repo)
+    return buttons
+
+
+def prepare_inline_results(query, items):
+    results = []
+    for item in items:
+        id = str(uuid4())
+        full_name = item["full_name"]
+        repo_description = item["description"]
+        language = item["language"]
+        stars = item["stargazers_count"]
+        thumbnail = item["owner"]["avatar_url"]
+        fork_count = item["forks_count"]
+        description = f"💻 {language}"
+        if stars > 0:
+            description += f" | ⭐️ {stars}"
+        if fork_count > 0:
+            description += f" | 🍴 {fork_count}"
+        inline_message = f"{description}\n{repo_description}"
+        text_message = (
+            f"<b>{full_name}</b>\n\n<i>{repo_description}</i>\n\n{description}"
+        )
+        repo_url = item["html_url"]
+        buttons = get_buttons(query=query, repo=repo_url)
+        buttons_dict[id] = buttons
+        message_dict[id] = text_message
+        full_name_dict[id] = full_name
+        results.append(
+            types.InlineQueryResultArticle(
+                id=id,
+                title=full_name,
+                thumbnail_url=thumbnail,
+                description=inline_message,
+                input_message_content=types.InputTextMessageContent(
+                    message_text=text_message, parse_mode="html"
+                ),
+                reply_markup=buttons,
+            )
+        )
+    return results
+
+
+def get_inline_data(query):
+    repos = search_repos(query)
+    if repos is None:
+        results = []
+    else:
+        results = prepare_inline_results(query, repos)
+    return results
+
+
+def get_latest_release_details(full_name):
+    data = get_release_details(full_name)
+    if data is not None and len(data) > 0:
+        item = data[0]
+        tag = item["tag_name"]
+        name = item["name"]
+        time = item["published_at"]
+        message = parse_text_for_tg_markdown(item["body"])
+        url = item["html_url"]
+        release_details = f"\n<b>Latest Release</b>\n\n{name}\nversion: {tag}\nTime: {time}\n\n<i>{message}</i>"
+        return release_details, url
+    else:
+        return None, None
+
+
+def get_message_url_and_buttons_for(id):
+    message = message_dict.get(id)
+    full_name = full_name_dict.get(id)
+    buttons = buttons_dict.get(id, types.InlineKeyboardMarkup(row_width=2))
+    release_info, url = get_latest_release_details(full_name)
+    btn_release = types.InlineKeyboardButton("Goto Download Page", url=url)
+    buttons.row(btn_release)
+    updated_text = (str(message) + "\n\n" + str(release_info))[:4095]
+    return updated_text, buttons
